6b28ec217f0062c1bea8c76c26b7bcc7fe15cd2c
Ben Shealy <bentshermann@gmail.com>
2018-10-08 12:21:14 -0400

    Added script to visualize sample distributions

---
+++ scripts/visualize.py
@@ -0,0 +1,30 @@
+import argparse
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+if __name__ == "__main__":
+	# parse command-line arguments
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-i", "--input", required=True, help="input expression matrix", dest="INPUT")
+	# TODO: --box-plot
+	# TODO: --dist
+
+	args = parser.parse_args()
+
+	# load input data
+	emx = pd.read_table(args.INPUT, index_col=0)
+
+	# plot sample distributions
+	_, ax = plt.subplots(figsize=(10, 10))
+
+	for colname in emx.columns:
+		x = emx[colname].dropna()
+		sns.distplot(x, hist=False, ax=ax)
+
+	plt.title("Sample Distributions: %s" % (args.INPUT))
+	plt.xlim(emx.min().min(), emx.max().max())
+	plt.xlabel("Expression Level")
+	plt.ylabel("Density")
+	plt.savefig("dist.png")
